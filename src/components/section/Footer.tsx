83f0ea06d0ad5df63b7fca9986fbe54515b69a7c
ruanvsrateira <vruan85@gmail.com>
2023-07-13 19:26:21 -0300

    fix: unutilized imports

--- src/components/section/Footer.tsx
+++ src/components/section/Footer.tsx
@@ -1,4 +1,4 @@
-import { FaFacebook, FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 import "./Footer.scss";
 import { MdEmail } from "react-icons/md";
 
